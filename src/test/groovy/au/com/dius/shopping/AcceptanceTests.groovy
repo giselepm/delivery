1ddb1d4052e9540042d61b93e4a8aa69d0784879
Gisele Paiva <gisele.paiva@gmail.com>
2017-03-09 16:06:50 +1100

    Refactor AcceptanceTests class to create setup method

--- src/test/groovy/au/com/dius/shopping/AcceptanceTests.groovy
+++ src/test/groovy/au/com/dius/shopping/AcceptanceTests.groovy
@@ -3,24 +3,25 @@ package au.com.dius.shopping
 import spock.lang.Specification
 
 class AcceptanceTests extends Specification {
+    private PricingRules pricingRules
+    Checkout checkout
+
+    void setup() {
+        pricingRules = new PricingRules()
+        checkout = new Checkout(pricingRules)
+    }
 
     def "When no product was scanned, the total is \$0"() {
-        when:
-        PricingRules pricingRules = new PricingRules()
-        Checkout co = new Checkout(pricingRules)
-
-        then:
-        "\$0.00" == co.total()
+        expect:
+        "\$0.00" == checkout.total()
     }
 
     def "When one VGA adapter is scanned, the total is \$30.00"() {
         when:
-        PricingRules pricingRules = new PricingRules()
-        Checkout co = new Checkout(pricingRules)
         Product vgaAdapter = new Product("vga", "VGA Adapter", 30.00)
-        co.scan(vgaAdapter)
+        checkout.scan(vgaAdapter)
 
         then:
-        "\$30.00" == co.total()
+        "\$30.00" == checkout.total()
     }
 }
