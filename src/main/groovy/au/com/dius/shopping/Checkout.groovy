9e562765bf8329af11bee0835cb160bbbec8e74c
Gisele Paiva <gisele.paiva@gmail.com>
2017-03-09 16:58:02 +1100

    Changed checkout to allow scanning 2 items

--- src/main/groovy/au/com/dius/shopping/Checkout.groovy
+++ src/main/groovy/au/com/dius/shopping/Checkout.groovy
@@ -1,17 +1,17 @@
 package au.com.dius.shopping
 
 class Checkout {
-    private String total = "\$0.00"
+    private BigDecimal total = 0.00
 
     Checkout(PricingRules pricingRules) {
 
     }
 
-    String total(){
-        total
+    String total() {
+        "\$${total}"
     }
 
     void scan(Product product) {
-        total = "\$${product.price}"
+        total += product.price
     }
 }
