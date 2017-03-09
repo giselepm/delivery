a9df03a9579df5cbc8ef73639967c86ae84f49e5
Gisele Paiva <gisele.paiva@gmail.com>
2017-03-09 18:57:44 +1100

    Changed total and scan methods to include only 2 appleTVs in the price when 3 are scanned

--- src/main/groovy/au/com/dius/shopping/Checkout.groovy
+++ src/main/groovy/au/com/dius/shopping/Checkout.groovy
@@ -2,16 +2,29 @@ package au.com.dius.shopping
 
 class Checkout {
     private BigDecimal total = 0.00
+    private List<Product> items = []
 
     Checkout(PricingRules pricingRules) {
 
     }
 
     String total() {
+        int appleTVCounter = 0
+        items.each { Product item ->
+            if (item.sku == "atv") {
+                appleTVCounter++
+            }
+            total += item.price
+        }
+
+        if (appleTVCounter >= 3) {
+            total -= 109.50
+        }
+
         "\$${total}"
     }
 
     void scan(Product product) {
-        total += product.price
+        items << product
     }
 }
