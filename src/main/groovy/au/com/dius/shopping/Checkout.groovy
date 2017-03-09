9d564ff7d00995bbc95ce069ecefd6af7ed38695
Gisele Paiva <gisele.paiva@gmail.com>
2017-03-10 10:11:11 +1100

    Changed total method to make test pass

--- src/main/groovy/au/com/dius/shopping/Checkout.groovy
+++ src/main/groovy/au/com/dius/shopping/Checkout.groovy
@@ -11,14 +11,18 @@ class Checkout {
     String total() {
         List<Product> appleTVItems = []
         List<Product> iPadItems = []
-        List<Product> otherItems = []
+        List<Product> macBookItems = []
+        List<Product> vgaAdapterItems = []
+
         items.each { Product item ->
             if (item.sku == "atv") {
                 appleTVItems << item
             } else if (item.sku == "ipd") {
                 iPadItems << item
-            } else {
-                otherItems << item
+            } else if (item.sku == "mbp") {
+                macBookItems << item
+            } else if (item.sku == "vga") {
+                vgaAdapterItems << item
             }
             total += item.price
         }
@@ -26,9 +30,14 @@ class Checkout {
         if (appleTVItems && appleTVItems.size() >= 3) {
             total -= ((int)(appleTVItems.size()) / 3) * 109.50
         }
+
         if (iPadItems && iPadItems.size() > 4) {
             total -= iPadItems.first().price * iPadItems.size()
             total += iPadItems.size() * 499.99
+        }
+
+        if (macBookItems && vgaAdapterItems) {
+            total -= vgaAdapterItems.size() * vgaAdapterItems.price
         }
 
         "\$${total}"
